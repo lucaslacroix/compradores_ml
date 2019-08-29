f043bf2e34129e2e16a57b64beaee1a4f4446ec6
Lucas Lacroix <lucas-lacroix@hotmail.com>
2019-08-29 00:40:48 -0300

    subir beta

---
+++ src/connect.js
@@ -0,0 +1,25 @@
+
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+//console.log(dotenv);
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+db.connect((err, args) => {
+    if (!err) {
+        console.log('SQL Conectado com sucesso!');
+        console.log(args);
+    } else {
+        console.log('Erro ao conectar SQL: ', JSON.stringify(err));
+    }
+});
+
+module.exports = db;
---
+++ src/routes.js
@@ -0,0 +1,97 @@
+const routes = require('express').Router();
+const db = require('./connect');
+const fs = require('fs');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+routes.get('/buscar-emails', async (req, res) => {
+    // let seller_id = '236088666';
+    // let access_token = 'APP_USR-8021611602487823-082821-644451b2ffbbdcbaafc041ac4baab966-236088666';
+    console.time('demorando');
+    db.query('SELECT contas.ID FROM contas WHERE contas.ATIVA = 1', (err, selectIdRes, field) => {
+        if (err) {
+            console.log('SELECT ERROR: ', err);
+        }
+
+        for (let dbIds in selectIdRes) {
+            db.query(`SELECT contas.ACCESSTOKEN FROM contas WHERE contas.ID = ${selectIdRes[dbIds].ID}`, async (err, dbAccessResult, field) => {
+                const { ID: seller_id } = selectIdRes[dbIds];
+                const { ACCESSTOKEN: access_token } = dbAccessResult[0];
+                if (err) {
+                    console.log('SELECT ERROR: ', err);
+                }
+                let emails = [];
+
+                if (access_token === 'DESATIVADO') return;
+
+                const ordersPackId = []
+
+                const ordersRes = await fetch(`https://api.mercadolibre.com/orders/search/recent?seller=${seller_id}&access_token=${access_token}`);
+
+                const { results } = await ordersRes.json();
+
+
+                for (let resu in results) {
+                    //console.log(results[resu]);
+                    const { pack_id } = results[resu];
+
+                    if (pack_id) {
+                        ordersPackId.push({ pack_id, buyer: { ...results[resu].buyer } });
+                    }
+                }
+
+                if (ordersPackId.length <= 0) return;
+
+
+                emails.push(...(await buscarPorOrderPackId(ordersPackId, seller_id, access_token)));
+                //console.log(seller_id, access_token);
+                //console.log(emails);
+                for (let email in emails) {
+                    const selectEmailsQuery = `SELECT desmascaradostmp.IDCOMPRADOR FROM desmascaradostmp WHERE desmascaradostmp.IDCOMPRADOR = '${emails[email].id}' AND desmascaradostmp.idConta = ${seller_id}`
+                    db.query(selectEmailsQuery, async (err, selectEmailsResult, field) => {
+                        if (err) {
+                            console.log('INSERT ERROR: ', err);
+                        }
+
+                        //console.log(selectEmailsResult);
+
+                        if (selectEmailsResult.length <= 0) {
+                            const insertQuery = `INSERT INTO desmascaradostmp (APELIDO, COMPLEMENTOTELEFONE, DDD, EMAIL, IDCOMPRADOR, NOME, NUMERODOCUMENTO, SOBRENOME, TELEFONE, TIPODOCUMENTO, idConta) VALUES ('${emails[email].nickname}', '${emails[email].phone.extension}', '${emails[email].phone.area_code}', '${emails[email].email}', '${emails[email].id}', '${emails[email].first_name}', '${emails[email].billing_info.doc_number}', '${emails[email].last_name}', '${emails[email].phone.number}', '${emails[email].billing_info.doc_type}', '${seller_id}');`
+                            db.query(insertQuery, async (err, insertResult, field) => {
+                                if (err) {
+                                    console.log('INSERT ERROR: ', err);
+                                }
+
+                                console.log('inseriu');
+                            });
+                        } else {
+                            console.log('já existe, não inserir');
+                        }
+                    });
+                }
+            })
+        }
+
+        return res.json({ ok: true });
+    })
+})
+
+async function buscarPorOrderPackId(arrPackIds, seller_id, access_token) {
+    let buyers = []
+
+    for (let id in arrPackIds) {
+        const res = await fetch(`https://api.mercadolibre.com/messages/packs/${arrPackIds[id].pack_id}/sellers/${seller_id}?access_token=${access_token}&limit=1`);
+        if (!res) return;
+
+        const results = await res.json();
+        //console.log(results);
+        const to = results && results.messages && results.messages[0] && results.messages[0].to ? results.messages[0].to : null;
+        if (to) {
+            buyers.push({ ...arrPackIds[id].buyer, email: to.email, user_id: to.user_id });
+        }
+    }
+    return buyers;
+}
+
+
+module.exports = routes;
